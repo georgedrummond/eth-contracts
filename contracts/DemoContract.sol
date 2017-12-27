f8976632a84f9846881f328835aa7ccb8dfc589c
George Drummond <georgedrummond@gmail.com>
2017-12-27 11:02:30 -0500

    Playing with transactions

--- contracts/DemoContract.sol
+++ contracts/DemoContract.sol
@@ -1,17 +1,51 @@
 pragma solidity ^0.4.4;
 
 contract DemoContract {
-  string public message;
+  // string public message;
+  // uint public balance;
+
+  // struct Depositor {
+  //   address delegate;
+  //   uint balance;
+  // }
+  //
+  // Depositor[] public depositors;
+
+  // address public seller;
+  uint public balance;
+
+  event EtherDeposited(address sender);
 
   function DemoContract() public {
-    message = "Old message";
+    balance = 0;
+  }
+    // seller = msg.sender;
+    // balance = 3;
+    // balance = msg.value;
+
+  function() public payable {
+    balance += msg.value;
+    EtherDeposited(msg.sender);
   }
 
-  function SetMessage(string _message) public {
-    message = _message;
-  }
+  // function deposit() public payable {
+  //   // balance = msg.value;
+  // }
 
-  function GetMessage() public view returns (string) {
-    return message;
-  }
+  // function SetMessage(string _message) public {
+  //   message = _message;
+  // }
+  //
+  // function GetMessage() public view returns (string) {
+  //   return message;
+  // }
+  //
+  // function GetBalance() public view returns(uint) {
+  //   return balance;
+  // }
+
+  // function deposit() public payable {
+  //   require(false);
+  //   // balance += msg.value;
+  // }
 }
--- test/demo_contract.js
+++ test/demo_contract.js
@@ -1,32 +1,71 @@
 var DemoContract = artifacts.require("DemoContract.sol");
 
 contract('DemoContract', function(accounts) {
-  it("should assert true", function(done) {
-    var demo_contract = DemoContract.deployed();
-    assert.isTrue(true);
-    done();
-  });
-
-  it("sets the welcome", function(done) {
+  // it("should assert true", function(done) {
+  //   var demo_contract = DemoContract.deployed();
+  //   assert.isTrue(true);
+  //   done();
+  // });
+  //
+  // it("sets the welcome", function(done) {
+  //   DemoContract.deployed().then(instance => {
+  //     return instance.GetMessage.call();
+  //   }).then(message => {
+  //     assert.equal(message, "Old message");
+  //     done();
+  //   });
+  // });
+  //
+  it('has some stuff', done => {
     DemoContract.deployed().then(instance => {
-      return instance.GetMessage.call();
-    }).then(message => {
-      assert.equal(message, "Old message");
-      done();
+      web3.eth.getBalance(accounts[0], (err, balance) => {
+        let eth = web3.fromWei(balance.toNumber(), 'ether');
+        assert.equal(eth, 98.2630808);
+        done();
+      });
     });
   });
 
-  it("allows message override", done => {
+  it('setting and reading balance', done => {
     var contract;
+
+    var account0 = accounts[0];
+    var account1 = accounts[1];
 
     DemoContract.deployed().then(instance => {
       contract = instance;
-      return contract.SetMessage("New message");
-    }).then(newMessage => {
-      return contract.GetMessage.call();
-    }).then(message => {
-      assert.equal(message, "New message");
+    //   return contract.GetBalance.call();
+    // }).then(balance => {
+    //   assert.equal(balance.toNumber(), 3);
+
+      var eth = web3.toWei(1.2, 'ether');
+
+      return contract.sendTransaction({
+        value: eth,
+        gas: 4712388,
+        from: account0,
+      });
+    }).then(() => {
+    //   return contract.GetBalance.call(account0);
+    // }).then(balance => {
+    //   assert.equal(2, balance);
       done();
-    });
+    })
   });
+
+  // describe("SetMessage", () => {
+  //   it("allows message override", done => {
+  //     var contract;
+  //
+  //     DemoContract.deployed().then(instance => {
+  //       contract = instance;
+  //       return contract.SetMessage("New message");
+  //     }).then(newMessage => {
+  //       return contract.GetMessage.call();
+  //     }).then(message => {
+  //       assert.equal(message, "New message");
+  //       done();
+  //     });
+  //   });
+  // });
 });
